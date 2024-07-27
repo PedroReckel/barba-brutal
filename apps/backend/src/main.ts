a024dc6f7021ede7edbca6861fc9a888f7e8cf77
PedroReckel <pedroreckel90@gmail.com>
2024-07-27 17:01:51 -0300

    Criado o backend

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true }); // Tem que usar o CORS igual a true para ele conseguir receber requisições de outras origens
   await app.listen(3001);
 }
 bootstrap();
